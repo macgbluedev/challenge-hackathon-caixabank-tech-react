47d98df09b8ea945a836352c651ed4fcaee6141d
Moises Croes <mcg-360@hotmail.com>
2022-05-28 12:10:56 +0200

    Include constructor in transaction model

--- src/contexts/neobanks/domain/entities/Transaction.ts
+++ src/contexts/neobanks/domain/entities/Transaction.ts
@@ -1,3 +1,14 @@
-export class Transaction {
+type ConstructorParams = {
     date: Date;
+}
+
+
+export class Transaction {
+    public date: Date;
+
+    constructor({
+        date
+    }: ConstructorParams) {
+        this.date = date
+    }
 }
