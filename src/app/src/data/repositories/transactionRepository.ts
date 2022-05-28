2fd5b998bf875a7900393ee4e44be9dc8e310b8b
Moises Croes <mcg-360@hotmail.com>
2022-05-28 19:23:31 +0200

    Scaffolding new react native blueprint with ts

--- src/app/src/data/repositories/transactionRepository.ts
+++
@@ -1,9 +0,0 @@
-import { Transaction } from '../../../../contexts/neobanks/domain/entities/Transaction';
-import { TransactionRepository } from '../../../../contexts/neobanks/domain/repositories/transactionRepository'
-
-export class TranstionRepositoryImpl implements TransactionRepository {
-    getTransactions(): Promise<Transaction[]> {
-        throw new Error('Method not implemented.');
-    }
-    
-}
